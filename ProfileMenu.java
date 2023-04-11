2fd9cc1dda1c05d6f526152433a6fb1938bf8640
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-11 22:20:59 +0330

    profile menu in view
    first release

---
+++ ProfileMenu.java
@@ -0,0 +1,43 @@
+package view;
+
+import model.User;
+
+public class ProfileMenu {
+    private User currentUser;
+
+    public void changeUsername(){
+
+    }
+
+    public void changeNickname(){
+
+    }
+
+    public void changePassword(){
+
+    }
+
+    public void changeEmail(){
+
+    }
+
+    public void changeSlogan(){
+
+    }
+
+    public void showHighscore(){
+
+    }
+
+    public void showRank(){
+
+    }
+
+    public void showSlogan(){
+
+    }
+
+    public void showDisplay(){
+
+    }
+}
