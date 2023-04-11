6b6c7b6c60a09c07e896f84e539e320894245d74
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-11 22:22:28 +0330

    profile menu controller
    first release

---
+++ ProfileMenuController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import model.User;
+import view.messages.profileMessages;
+
+public class ProfileMenuController {
+    public static profileMessages changeUsername(User user, String username){
+        return null;
+    }
+
+    public static profileMessages changeNickname(User user, String nickname){
+        return null;
+    }
+
+    public static profileMessages changePassword(User user, String password){
+        return null;
+    }
+
+    public static profileMessages changeEmail(User user, String email){
+        return null;
+    }
+
+    public static profileMessages changeSlogan(User user, String slogan){
+        return null;
+    }
+}
