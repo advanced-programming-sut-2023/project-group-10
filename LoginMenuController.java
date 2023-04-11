f4964714a0a39818f4f18b3fbbfc277ac496c316
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-11 22:21:42 +0330

    login menu controller
    first release

---
+++ LoginMenuController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.User;
+import view.messages.loginMessages;
+
+public class LoginMenuController {
+    public static loginMessages login(String username, String password){
+        return null;
+    }
+
+    public static loginMessages forgetPassword(User user, String answer){
+        return null;
+    }
+}
