7f56cb26faa5c156ce3f5fba4e9fbe65ecbf8e56
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-11 22:20:04 +0330

    login menu in view
    first release

---
+++ LoginMenu.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.util.regex.Matcher;
+
+public class LoginMenu {
+
+    public void login(Matcher matcher){
+
+    }
+
+    public void forgetPassword(Matcher matcher){
+
+    }
+}
