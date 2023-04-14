d8f8bbde12b44af8544c79b2d123d1d6141cbc3a
MehrazinM <mehrazin82@gmail.com>
2023-04-14 15:53:19 +0330

    unitMenu

---
+++ view/UnitMenu.java
@@ -0,0 +1,46 @@
+package main.java.view;
+
+import main.java.model.mapDirections;
+
+import java.util.regex.Matcher;
+
+public class UnitMenu {
+    public void run(){
+
+    }
+    public static void moveUnit(Matcher matcher){
+
+    }
+
+    public static void patrolUnit(Matcher matcher) {
+
+    }
+    //
+    public static void setState(Matcher matcher) {
+
+    }
+
+    public static void airAttack(Matcher matcher) {
+
+    }
+
+    public static void attackEnemy(Matcher matcher) {
+
+    }
+
+    public static void pourOil(Matcher matcher) {
+
+    }
+
+    public static void digTunnel(Matcher matcher) {
+
+    }
+
+    public static void build(Matcher matcher) {
+
+    }
+
+    public static void disband() {
+
+    }
+}
