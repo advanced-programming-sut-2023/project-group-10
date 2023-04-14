4f41d66df4d6d9098169aad97ecb136e40b05909
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 15:29:10 +0330

    Create RandomGenerator.java

---
+++ model/utils/RandomGenerator.java
@@ -0,0 +1,18 @@
+package model.utils;
+import model.util.Random;
+
+public class RandomGenerator {
+    private static String[] slogans={" ", " "," "};
+    public String getRandomPassword(){
+        return null;
+    }
+    public  static String getRandomSlogan(){
+        return null;
+    }
+    public static String randomCaptchaCode(){
+        return null;
+    }
+
+
+
+}
