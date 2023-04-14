cd6327bc7f13540b0775cb91338f6bcfdcb21f07
MehrazinM <mehrazin82@gmail.com>
2023-04-14 12:23:14 +0330

    utils added

---
+++ utils/RandomGenerator.java
@@ -0,0 +1,18 @@
+package main.java.model.utils;
+import java.util.Random;
+
+public class RandomGenerator {
+    private static String[] slogans={" ", " "," "};
+    public String getRandomPassword(){
+        return null;
+    }
+    public  static String getRandomSlogan(){
+        return null;
+    }
+    public static String randomCaptchaCode(){
+        return null;
+    }
+
+
+
+}
