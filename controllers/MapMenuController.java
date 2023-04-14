5b3a0c1b25e17c0efd06c0a23d4eb9e515e02378
MehrazinM <mehrazin82@gmail.com>
2023-04-14 14:26:43 +0330

    mvc

---
+++ controllers/MapMenuController.java
@@ -0,0 +1,21 @@
+package main.java.controller;
+import kotlin.Pair;
+import main.java.model.mapDirections;
+
+public class MapMenuController {
+
+    public main.java.model.environment.Map showMap(int row, int column) {
+        return null;
+    }
+// TODO: enum for directions?
+    @SafeVarargs
+    public final void moveMap(Pair<mapDirections, Integer>[]... directionsAndValues) {
+
+    }
+
+    // TODO: decide about  the type
+    public String showDetails(int row, int column) {
+        return null;
+    }
+
+}
