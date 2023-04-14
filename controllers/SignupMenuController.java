633c56dc7a3ee3d9db18b3a90756c050526f70c7
MehrazinM <mehrazin82@gmail.com>
2023-04-14 13:11:59 +0330

    signupMenu review

---
+++ controllers/SignupMenuController.java
@@ -0,0 +1,37 @@
+package main.java.controller;
+
+import main.java.model.SecurityQuestion;
+import main.java.view.enums.messages.SignUpMenuMessages;
+
+import java.util.ArrayList;
+
+public class SignupMenuController {
+    public static void run() {
+
+    }
+
+    public static SignUpMenuMessages createUser(String username, String password, String passwordConfirmation, String email,
+                                                String nickname, String slogan) {
+        return null;
+    }
+
+
+    //TODO: decide abt the type
+    public boolean checkEmailFormat(String emailAddress) {
+        return true;
+    }
+
+    public String suggestNewUsername(String username) {
+        return null;
+    }
+
+    public ArrayList<SecurityQuestion> showQuestions() {
+        return null;
+    }
+
+    public static SignUpMenuMessages pickSecurityQuestion(String questionNumber, String answer, String answerConfirmation) {
+        return null;
+    }
+
+
+}
---
+++ view/SignupMenu.java
@@ -0,0 +1,17 @@
+package main.java.view;
+
+import java.util.regex.Matcher;
+
+public class SignupMenu {
+   public static void run(){
+
+   }
+   public static void register(Matcher matcher){
+
+   }
+   public static void securityQuestion(Matcher matcher){
+
+   }
+
+
+}
