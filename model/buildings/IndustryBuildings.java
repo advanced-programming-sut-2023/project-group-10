2789c4cfc1582221ff18fb426dc56f3528c3ed82
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 17:47:10 +0330

    Update IndustryBuildings.java

--- model/buildings/IndustryBuildings.java
+++ model/buildings/IndustryBuildings.java
@@ -1,22 +1,11 @@
 public class IndustryBuildings extends Buildings{
-    private String name;
-    private int hitPoint;
     private int rate;
     private int capacity;
 
     public IndustryBuildings(String name, int hitPoint, int rate, int capacity){
-        this.name = name;
-        this.hitPoint = hitPoint;
+        super(name, hitPoint);
         this.rate = rate;
         this.capacity = capacity;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
     }
 
     public int getRate() {
@@ -25,10 +14,6 @@ public class IndustryBuildings extends Buildings{
 
     public int getCapacity() {
         return capacity;
-    }
-
-    public void setHitPoint(int hitPoint) {
-        this.hitPoint = hitPoint;
     }
 
     public void setRate(int rate) {
