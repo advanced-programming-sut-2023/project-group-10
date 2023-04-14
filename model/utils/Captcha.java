acfc5fd21a4fa2e6986bf93e41cfa934aae68060
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 15:28:26 +0330

    Create Captcha.java

---
+++ model/utils/Captcha.java
@@ -0,0 +1,15 @@
+package model.utils;
+
+public class Captcha {
+    private  String answer;
+
+    public Captcha() {
+        answer=RandomGenerator.randomCaptchaCode();
+    }
+    public boolean checkAnswer(String answer){
+        return this.answer.equals(answer);
+    }
+    public String getImage(){
+        return null;
+    }
+}
