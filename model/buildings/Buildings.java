04fd1ee9d495af7dac6418b04044371aca4595ea
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 17:46:08 +0330

    Update Buildings.java

--- model/buildings/Buildings.java
+++ model/buildings/Buildings.java
@@ -1,4 +1,25 @@
 public class Buildings {
+
+    protected String name;
+    protected int hitPoint;
+
+    public Buildings(String name, int hitPoint){
+        this.name = name;
+        this.hitPoint = hitPoint;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+
     public void finalizeBuild(Government government, int woodCost, int stoneCost,
                               int goldCost, int popularityIncrease){
 
