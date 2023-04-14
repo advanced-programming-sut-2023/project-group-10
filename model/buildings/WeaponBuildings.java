2fecf2eda45176672e93c9d541fef529fe9f99a1
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 17:47:54 +0330

    Update WeaponBuildings.java

--- model/buildings/WeaponBuildings.java
+++ model/buildings/WeaponBuildings.java
@@ -1,22 +1,11 @@
 public class WeaponBuildings extends Buildings{
-    private String name;
-    private int hitPoint;
     private int materials;
     private int productionRate;
 
     public WeaponBuildings(String name, int hitPoint, int materials, int productionRate){
-        this.name = name;
-        this.hitPoint = hitPoint;
+        super(name, hitPoint);
         this.materials = materials;
         this.productionRate = productionRate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
     }
 
     public int getMaterials() {
@@ -25,10 +14,6 @@ public class WeaponBuildings extends Buildings{
 
     public int getProductionRate() {
         return productionRate;
-    }
-
-    public void setHitPoint(int hitPoint) {
-        this.hitPoint = hitPoint;
     }
 
     public void setMaterials(int materials) {
