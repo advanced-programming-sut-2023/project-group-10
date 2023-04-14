06641a708656a9749217de7ced0b28d92b1d1684
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 15:44:03 +0330

    Update UnitMenuController.java

--- controller/UnitMenuController.java
+++ controller/UnitMenuController.java
@@ -7,31 +7,31 @@ import java.util.regex.Matcher;
 
 public class UnitMenuController {
 
-    public static UnitMenuMessages moveUnit(Matcher matcher) {
+    public static UnitMenuMessages moveUnit(int x, int y) {
         return null;
     }
 
-    public static UnitMenuMessages patrolUnit(Matcher matcher) {
+    public static UnitMenuMessages patrolUnit(int x1, int y1, int x2, int y2) {
         return null;
     }
 
-    public static UnitMenuMessages setState(Matcher matcher) {
+    public static UnitMenuMessages setState(int x, int y, String state) {
         return null;
     }
 
-    public static UnitMenuMessages attack(Matcher matcher) {
+    public static UnitMenuMessages attack(int x, int y) {
         return null;
     }
 
-    public static UnitMenuMessages pourOil(Matcher matcher) {
+    public static UnitMenuMessages pourOil(String direction) {
         return null;
     }
 
-    public static UnitMenuMessages digTunnel(Matcher matcher) {
+    public static UnitMenuMessages digTunnel(int x, int y) {
         return null;
     }
 
-    public static UnitMenuMessages build(Matcher matcher) {
+    public static UnitMenuMessages build(String equipmentName) {
         return null;
     }
     public static UnitMenuMessages disband(){
