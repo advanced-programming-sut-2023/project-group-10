ca6a09117896b10ac72e3989ac943b5bcb537fca
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 15:08:46 +0330

    move shop menu controller

--- controllers/ShopMenuController.java
+++ controllers/ShopMenuController.java
@@ -3,16 +3,12 @@ package main.java.controller;
 import main.java.view.enums.messages.ShopMenuMessages;
 
 public class ShopMenuController {
-    public static void run() {
-
-    }
+    
     public ShopMenuMessages buy(){
         return null;
     }
+    
     public ShopMenuMessages sell(){
         return null;
     }
-
-
-
 }
