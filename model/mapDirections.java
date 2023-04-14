afa4c212adf562bae2fe3bcb41024b333eac7b8c
MehrazinM <mehrazin82@gmail.com>
2023-04-14 14:29:05 +0330

    directions

---
+++ model/mapDirections.java
@@ -0,0 +1,21 @@
+package main.java.model;
+
+public enum mapDirections {
+    UP("up",0,1),
+    DOWN("down",0,-1),
+    RIGHT("up",1,0),
+    LEFT("up",-1,0);
+
+
+    public final String name;
+    public final int horizontalChange;
+    public final int verticalChange;
+
+    mapDirections(String name, int horizontalChange, int verticalChange) {
+        this.name = name;
+        this.horizontalChange = horizontalChange;
+        this.verticalChange = verticalChange;
+    }
+
+
+}
---
+++ view/MapMenu.java
@@ -0,0 +1,21 @@
+package main.java.view;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.regex.Matcher;
+
+public class MapMenu {
+    public void run(){
+
+    }
+    //TODO: type check
+    public void showMap(Matcher matcher){
+
+    }
+    public void showDetails(Matcher matcher){
+
+    }
+    public void moveMap(Matcher matcher){
+
+    }
+}
