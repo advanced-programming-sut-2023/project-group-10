3fbfc4bbf0a90cee70a3589c4d28f01bd2c1fcd1
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 17:47:36 +0330

    Update TownBuildings.java

--- model/buildings/TownBuildings.java
+++ model/buildings/TownBuildings.java
@@ -1,21 +1,6 @@
 public class TownBuildings extends Buildings{
-    private String name;
-    private int hitPoint;
 
     public TownBuildings(String name, int hitPoint){
-        this.name = name;
-        this.hitPoint = hitPoint;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
-    }
-
-    public void setHitPoint(int hitPoint) {
-        this.hitPoint = hitPoint;
+        super(name, hitPoint);
     }
 }
