ac2debe32a56b2bb6ecf7958dfb0a63c5df738ea
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-14 17:46:54 +0330

    Update FoodProcessingBuildings.java

--- model/buildings/FoodProcessingBuildings.java
+++ model/buildings/FoodProcessingBuildings.java
@@ -1,28 +1,13 @@
 public class FoodProcessingBuildings extends Buildings{
-    private String name;
-    private int hitPoint;
     private int rate;
 
     public FoodProcessingBuildings(String name, int hitPoint, int rate){
-        this.name = name;
-        this.hitPoint = hitPoint;
+        super(name, hitPoint);
         this.rate = rate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHitPoint() {
-        return hitPoint;
     }
 
     public int getRate() {
         return rate;
-    }
-
-    public void setHitPoint(int hitPoint) {
-        this.hitPoint = hitPoint;
     }
 
     public void setRate(int rate) {
