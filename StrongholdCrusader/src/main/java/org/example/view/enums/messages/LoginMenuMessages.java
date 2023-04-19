de9a07fadafee84cb1a2f0f5b1d7ccf676a2810f
RozhTagh <rozhintn@gmail.com>
2023-04-19 14:00:48 +0330

    update enum in login menu message

--- StrongholdCrusader/src/main/java/org/example/view/enums/messages/LoginMenuMessages.java
+++ StrongholdCrusader/src/main/java/org/example/view/enums/messages/LoginMenuMessages.java
@@ -3,5 +3,7 @@ package org.example.view.enums.messages;
 public enum LoginMenuMessages {
     USERNAME_NOT_EXIST,
     WRONG_PASSWORD,
-    LOGIN_SUCCESSFUL;
+    LOGIN_SUCCESSFUL,
+    SECURITY_ANSWER_WRONG,
+    SECURITY_ANSWER_CORRECT;
 }
