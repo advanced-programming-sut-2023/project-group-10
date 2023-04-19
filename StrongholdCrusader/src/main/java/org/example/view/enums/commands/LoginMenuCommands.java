df4ce6440ab1873e4c5ca0d371e6d9465dec1c11
RozhTagh <rozhintn@gmail.com>
2023-04-19 13:58:06 +0330

    update regex in login menu commands

--- StrongholdCrusader/src/main/java/org/example/view/enums/commands/LoginMenuCommands.java
+++ StrongholdCrusader/src/main/java/org/example/view/enums/commands/LoginMenuCommands.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum LoginMenuCommands {
-    LOGIN("^\\s*user\\s+login\\s+-u\\s+(?<username>.+)\\s+-p\\s+(?<password>.+)\\s*$"),
+    LOGIN("^\\s*user\\s+login\\s+-u\\s+(?<username>.+)\\s+-p\\s+(?<password>.+)\\s*(\\s+?<stayloggedin>--stay-logged-in)?\\s*$"),
     FORGET_PASSWORD("^\\s*forgot\\s+my\\s+password\\s+-u\\s+(?<username>.+)\\s*$"),
     LOGOUT("^\\s*user\\s+logout\\s*$");
 
