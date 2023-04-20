ab49aa34304f1d3fdacabfefede8ce55428228f5
yasna nsh <y.noushirvani@gmail.com>
2023-04-20 19:01:47 +0330

    Add cost data to siege equipment types

--- StrongholdCrusader/src/main/java/org/example/model/game/units/unitconstants/MilitaryEquipmentRole.java
+++ StrongholdCrusader/src/main/java/org/example/model/game/units/unitconstants/MilitaryEquipmentRole.java
@@ -7,12 +7,12 @@ public class MilitaryEquipmentRole extends MilitaryUnitRole {
 
     static {
         //TODO: add siege equipment
-        new MilitaryEquipmentRole(RoleName.PORTABLE_SHIELD, defaultHitPoint, Quality.LOW, Quality.ZERO, Quality.ZERO, Quality.ZERO, 0, 1, 1);
-        new MilitaryEquipmentRole(RoleName.BATTERING_RAM, defaultHitPoint, Quality.LOW, Quality.HIGH, Quality.ZERO, Quality.EXTREMELY_HIGH, 0, 4, 2);
-        new MilitaryEquipmentRole(RoleName.SIEGE_TOWER, defaultHitPoint, Quality.LOW, Quality.ZERO, Quality.ZERO, Quality.ZERO, 0, 4, 3);
-        new MilitaryEquipmentRole(RoleName.CATAPULT, defaultHitPoint, Quality.LOW, Quality.AVERAGE, Quality.AVERAGE, Quality.VERY_HIGH, 0, 2, 2);
-        new MilitaryEquipmentRole(RoleName.TREBUCHET, defaultHitPoint, Quality.ZERO, Quality.HIGH, Quality.VERY_HIGH, Quality.AVERAGE, 0, 3, 2);
-        new MilitaryEquipmentRole(RoleName.FIRE_BALLISTA, defaultHitPoint, Quality.LOW, Quality.VERY_HIGH, Quality.HIGH, Quality.VERY_HIGH, 0, 2, 2);
+        new MilitaryEquipmentRole(RoleName.PORTABLE_SHIELD, defaultHitPoint, Quality.LOW, Quality.ZERO, Quality.ZERO, Quality.ZERO, 5, 1, 1);
+        new MilitaryEquipmentRole(RoleName.BATTERING_RAM, defaultHitPoint, Quality.LOW, Quality.HIGH, Quality.ZERO, Quality.EXTREMELY_HIGH, 150, 4, 2);
+        new MilitaryEquipmentRole(RoleName.SIEGE_TOWER, defaultHitPoint, Quality.LOW, Quality.ZERO, Quality.ZERO, Quality.ZERO, 150, 4, 3);
+        new MilitaryEquipmentRole(RoleName.CATAPULT, defaultHitPoint, Quality.LOW, Quality.AVERAGE, Quality.AVERAGE, Quality.VERY_HIGH, 150, 2, 2);
+        new MilitaryEquipmentRole(RoleName.TREBUCHET, defaultHitPoint, Quality.ZERO, Quality.HIGH, Quality.VERY_HIGH, Quality.AVERAGE, 150, 3, 2);
+        new MilitaryEquipmentRole(RoleName.FIRE_BALLISTA, defaultHitPoint, Quality.LOW, Quality.VERY_HIGH, Quality.HIGH, Quality.VERY_HIGH, 150, 2, 2);
     }
 
     private MilitaryEquipmentRole(RoleName name, int maxHitPoint, Quality speed, Quality attackRating, Quality attackRange, Quality accuracy, int cost, int numberOfEngineersNeeded, int buildTime) {
