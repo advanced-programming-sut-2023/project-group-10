e6c27d71fcbcacf52f760a8727ad4a255c09106e
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-22 17:47:18 +0330

    updated input separator function

--- StrongholdCrusader/src/main/java/org/example/model/utils/InputProcessor.java
+++ StrongholdCrusader/src/main/java/org/example/model/utils/InputProcessor.java
@@ -7,12 +7,23 @@ import java.util.regex.Pattern;
 
 public class InputProcessor {
     public static HashMap<String, String> separateInput(String input) {
-        Pattern pattern = Pattern.compile("(-(?<option>\S+)\s*(?<argument>".*"|[^\s\-]*))");
+        Pattern pattern = Pattern.compile("\"[^\"]+\"|\\S+");
         Matcher matcher = pattern.matcher(input);
         HashMap<String, String> optArg = new HashMap<>();
+        ArrayList<String> arrayList = new ArrayList<>();
 
         while (matcher.find()) {
-            optArg.put(matcher.group("option"), matcher.group("argument"));
+            arrayList.add(matcher.group());
+        }
+
+        for(int i = 0; i < arrayList.size(); i++){
+            if(arrayList.get(i).startsWith("-") && (i == arrayList.size()-1 || arrayList.get(i+1).startsWith("-"))){
+                optArg.put(arrayList.get(i), null);
+            }
+
+            else if(arrayList.get(i).startsWith("-") && !arrayList.get(i+1).startsWith("-")){
+                optArg.put(arrayList.get(i), arrayList.get(i+1));
+            }
         }
 
         return optArg;
