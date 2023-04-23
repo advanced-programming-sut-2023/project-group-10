b91eb600fb9cfdb0a2992a963c9b5ab14f4ebda6
yasna nsh <y.noushirvani@gmail.com>
2023-04-23 15:12:14 +0330

    Update trade menu input

--- StrongholdCrusader/src/main/java/org/example/view/TradeMenu.java
+++ StrongholdCrusader/src/main/java/org/example/view/TradeMenu.java
@@ -1,106 +1,91 @@
 package org.example.view;
 
-import org.apache.commons.cli.*;
 import org.example.controller.TradeMenuController;
+import org.example.model.utils.InputProcessor;
 import org.example.view.enums.commands.TradeMenuCommands;
 import org.example.view.enums.messages.TradeMenuMessages;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 
 public class TradeMenu extends Menu {
     public static void run() {
+        //TODO: show notification for trades
         Scanner scanner = new Scanner(System.in);
-        Matcher matcher;
+        String input;
         while (true) {
-            if ((matcher = TradeMenuCommands.getMatcher(scanner.nextLine(), TradeMenuCommands.SEND_REQUEST)) != null)
-                sendRequest(matcher);
-            else if ((matcher = TradeMenuCommands.getMatcher(scanner.nextLine(), TradeMenuCommands.ACCEPT)) != null) {
-                acceptRequest(matcher);
-            }
-            else if ((matcher = TradeMenuCommands.getMatcher(scanner.nextLine(), TradeMenuCommands.TRADE_LIST)) != null) {
-                showTradeList();
-            }
-            else if ((matcher = TradeMenuCommands.getMatcher(scanner.nextLine(), TradeMenuCommands.HISTORY)) != null) {
-                showHistory();
-            }
-            else if ((matcher = TradeMenuCommands.getMatcher(scanner.nextLine(), TradeMenuCommands.BACK)) != null) {
-                // go to game menu
-            }
-
-            else {
-                System.out.println("Invalid command!");
-
-            }
+            input = scanner.nextLine();
+            if (TradeMenuCommands.getMatcher(input, TradeMenuCommands.SEND_REQUEST) != null) sendRequest(input);
+            else if (TradeMenuCommands.getMatcher(input, TradeMenuCommands.ACCEPT) != null) acceptRequest(input);
+            else if (TradeMenuCommands.getMatcher(input, TradeMenuCommands.TRADE_LIST) != null) showTradeList();
+            else if (TradeMenuCommands.getMatcher(input, TradeMenuCommands.HISTORY) != null) showHistory();
+            else if (TradeMenuCommands.getMatcher(input, TradeMenuCommands.BACK) != null) return;
+            else System.out.println("Invalid command!");
         }
-
-    }
-    //TODO: change the whole controller
-
-    private static void sendRequest(Matcher matcher) {
-
-        String arguments = matcher.replaceAll("");
-        Scanner scanner = new Scanner(System.in);
-
-        Options options = new Options();
-        Option typeOption = Option.builder().argName("t").longOpt("resourceType").hasArgs().required().desc("resourceType").build();
-        Option amountOption = Option.builder().argName("a").longOpt("resourceAmount").hasArgs().required().desc("resourceAmount").build();
-        Option priceOption = Option.builder().argName("p").longOpt("price").hasArgs().required().desc("price").build();
-        Option messageOption = Option.builder().argName("m").longOpt("message").hasArgs().required().desc("message").build();
-        Option recipientOption = Option.builder().argName("r").longOpt("recipientId").hasArgs().required().desc("recipientId").build();
-        options.addOption(typeOption);
-        options.addOption(amountOption);
-        options.addOption(priceOption);
-        options.addOption(messageOption);
-        options.addOption(recipientOption);
-
-        CommandLineParser parser = new DefaultParser(false);
-        try {
-            CommandLine cmd = parser.parse(options, args);
-
-            if (cmd.getOptionValues(typeOption).length != 1)
-                throw new ParseException("error: resource type must have only one argument");
-            String resourceType = cmd.getOptionValue(typeOption);
-
-            if (cmd.getOptionValues(amountOption).length != 1)
-                throw new ParseException("error: amount must have only one argument");
-            int amount = Integer.parseInt(cmd.getOptionValue(amountOption));
-
-
-            if (cmd.getOptionValues(priceOption).length != 1)
-                throw new ParseException("error: price must have only one argument");
-            int price = Integer.parseInt(cmd.getOptionValue(priceOption));
-
-
-            if (cmd.getOptionValues(messageOption).length != 1)
-                throw new ParseException("error: message must have only one argument");
-            String message = cmd.getOptionValue(messageOption);
-
-
-            if (cmd.getOptionValues(recipientOption).length != 1)
-                throw new ParseException("error: recipient ID must have only one argument");
-            String recipientId = cmd.getOptionValue(recipientOption);
-            TradeMenuMessages tradeMessage = TradeMenuController.sendRequest(resourceType, amount, price, message, recipientId);
-            switch (tradeMessage) {
-                case INSUFFICIENT_STOCK:
-                    System.out.println("You don't have enough resources to donate!");
-                    break;
-                case INVALID_USER:
-                    System.out.println("There is no user with this id in the current battle!");
-                    break;
-                case INVALID_TYPE:
-                    System.out.println("There is no resource with this type");
-                    break;
-            }
-
-
-        } catch (ParseException exception) {
-            printMessage(ExceptionMessages.getMessageFromException(exception));
-        }
-
     }
 
-    private static void acceptRequest(Matcher matcher) {
+    private static void sendRequest(String input) {
+        // process input
+        HashMap<String, String> options = InputProcessor.separateInput(input);
+        String resourceType = "";
+        String resourceAmountString = "";
+        int resourceAmount = 0;
+        String price = "";
+        String message = "";
+        String recipientId = "";
+        for (Map.Entry<String, String> option : options.entrySet()) {
+            switch (option.getKey()) {
+                case "-t":
+                    resourceType = option.getValue();
+                    break;
+                case "-a":
+                    resourceAmountString = option.getValue();
+                    break;
+                case "-p":
+                    price = option.getValue();
+                    break;
+                case "-m":
+                    message = option.getValue();
+                    break;
+                case "-r":
+                    recipientId = option.getValue();
+                    break;
+                default:
+                    System.out.println("invalid option");
+            }
+        }
+        if (resourceType == null | resourceAmountString == null || price == null || message == null || recipientId == null) {
+            System.out.println("empty field");
+            return;
+        }
+        if (resourceType.isEmpty() || resourceAmountString.isEmpty() || price.isEmpty() || message.isEmpty() || recipientId.isEmpty()) {
+            System.out.println("missing argument");
+            return;
+        }
+        if (!resourceAmountString.matches("\\d+")) {
+            System.out.println("invalid amount");
+            return;
+        } else resourceAmount = Integer.parseInt(resourceAmountString);
+
+        TradeMenuMessages tradeMessage = TradeMenuController.sendRequest(resourceType, resourceAmount, price, message, recipientId);
+        switch (tradeMessage) {
+            case INSUFFICIENT_STOCK:
+                System.out.println("You don't have enough resources to donate!");
+                break;
+            case INVALID_USER:
+                System.out.println("There is no user with this id in the current battle!");
+                break;
+            case INVALID_TYPE:
+                System.out.println("There is no resource with this type");
+                break;
+            case TRADE_ADDED_TO_TRADELIST:
+                System.out.println("trade was added successfully");
+                break;
+        }
+    }
+
+    private static void acceptRequest(String input) {
 
     }
 
@@ -110,7 +95,5 @@ public class TradeMenu extends Menu {
 
     private static void showTradeList() {
         System.out.print(TradeMenuController.showTradeList());
-
     }
-
 }
