576e70f5f2028ea50da5983be5543f7835c83cdf
MehrazinM <mehrazin82@gmail.com>
2023-04-23 22:30:52 +0330

    popularity(incomplete)

--- StrongholdCrusader/src/main/java/org/example/controller/GameMenuController.java
+++ StrongholdCrusader/src/main/java/org/example/controller/GameMenuController.java
@@ -1,43 +1,100 @@
 package org.example.controller;
 
+import org.example.model.Stronghold;
 import org.example.model.User;
+import org.example.model.game.Government;
+import org.example.model.game.Item;
 import org.example.view.enums.messages.GameMenuMessages;
 
-public class GameMenuController {
-    public static void chooseMapSize(int length) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class GameMenuController {
+    public static User currentPlayer() {
+        return Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getOwner();
+    }
+
+    public static String showPopularityFactors() {
+        String show = "";
+        HashMap<String, Integer> popularityFactors = Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getPopularityFactors();
+        for (Map.Entry<String, Integer> factors : popularityFactors.entrySet()) {
+            show = show.concat(factors.getKey() + " : " + factors.getValue() + "\n");
+        }
+        return show;
     }
 
     public static int roundsPlayed() {
-        return 0;
+
+        return Stronghold.getCurrentBattle().getTurnsPassed();
     }
 
-    public static User currentPlayer() {
-        return null;
+    public static String showPlayers() {
+        String players = "";
+        for (Government government : Stronghold.getCurrentBattle().getGovernments()) {
+            players = players.concat("player: " + government.getOwner().getNickname() + "is playing (username: " + government.getOwner().getUsername() + " )\n");
+        }
+        return players;
     }
 
-    public static void applyChanges() {
+    public static int showPopularity() {
+        return Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getPopularity();
     }
 
+    public static String showFoodList() {
+        String foodList = "";
 
-    public static String showPopularityFactors() {
-        return null;
+        for (Map.Entry<Item, Integer> list : Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFoodList().entrySet()) {
+            foodList = foodList.concat("Food : " + list.getKey() + " Amount : " + list.getValue() + "\n");
+        }
+        return foodList;
     }
 
-    public static void setFoodRate(int foodRate) {
+    //ATTENTION: I gave the modified rate to gov
+    public static GameMenuMessages setFoodRate(int foodRate) {
+        if (foodRate < -2 || foodRate > 2)
+            return GameMenuMessages.INVALID_FOOD_RATE;
+        //TODO: feed people by foodRate+2
+        Stronghold.getCurrentBattle().getGovernmentAboutToPlay().setFoodRate(foodRate);
+        //TODO: affect it on popularity!
+        return GameMenuMessages.SET_FOOD_RATE_SUCCESSFUL;
 
     }
 
     public static GameMenuMessages setTaxRate(int taxRate) {
-        return null;
+        if (taxRate < -3 || taxRate > 8)
+            return GameMenuMessages.INVALID_TAX_RATE;
+        Stronghold.getCurrentBattle().getGovernmentAboutToPlay().setTaxRate(taxRate);
+        return GameMenuMessages.SET_FOOD_TAX_SUCCESSFUL;
+
     }
 
     public static GameMenuMessages setFearRate(int fearRate) {
-        return null;
+        if (fearRate < -5 || fearRate > 5)
+            return GameMenuMessages.INVALID_TAX_RATE;
+        Stronghold.getCurrentBattle().getGovernmentAboutToPlay().setTaxRate(fearRate);
+        return GameMenuMessages.SET_FOOD_TAX_SUCCESSFUL;
     }
 
-    public User[] getPlayers() {
-        return null;
+
+    public static int showTaxRate() {
+        return Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getTaxRate();
+    }
+
+    public static int showFoodRate() {
+        return Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFoodRate();
+    }
+
+    public static int showFearRate() {
+        return Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFearRate();
+    }
+
+
+    public static void chooseMapSize(int length) {
+
+    }
+
+
+    public static void applyChanges() {
     }
 
 
@@ -65,8 +122,6 @@ public class GameMenuController {
     }
 
 
-
-
     public static GameMenuMessages setTexture(String landType, int row, int column) {
         return null;
     }
@@ -80,19 +135,19 @@ public class GameMenuController {
         return null;
     }
 
-    public static GameMenuMessages dropRock(int row, int column,String direction) {
+    public static GameMenuMessages dropRock(int row, int column, String direction) {
         return null;
     }
 
-    public static GameMenuMessages dropTree(int row, int column,String type) {
+    public static GameMenuMessages dropTree(int row, int column, String type) {
         return null;
     }
 
-    public static GameMenuMessages endTurn(){
+    public static GameMenuMessages endTurn() {
         return null;
     }
 
-    public static GameMenuMessages leaveGame(){
+    public static GameMenuMessages leaveGame() {
         return null;
     }
 }
--- StrongholdCrusader/src/main/java/org/example/view/GameMenu.java
+++ StrongholdCrusader/src/main/java/org/example/view/GameMenu.java
@@ -1,44 +1,131 @@
 package org.example.view;
 
+import org.example.controller.GameMenuController;
+import org.example.model.User;
+import org.example.model.utils.InputProcessor;
+import org.example.view.enums.messages.GameMenuMessages;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 
 public class GameMenu {
-    public static void run(){
+    public static void run() {
 
     }
 
-    private static void endTurn() {}
-
-    private static void leaveGame() {}
-
-    private static void showRoundsPlayed() {
-
-
-    }
-
+    // Is importing OK?
     private static void showCurrentPlayer() {
+        User player = GameMenuController.currentPlayer();
+        System.out.println("player \" " + player.getNickname() + "\" with username : " + player.getUsername() + "is about to play!");
     }
 
     private static void showPopularityFactors() {
-
-
+        System.out.println(GameMenuController.showPopularityFactors());
     }
 
-    private static void setFoodRate(Matcher matcher) {
+    private static void showRoundsPlayed() {
+        System.out.println(GameMenuController.roundsPlayed());
 
-    }
-
-    private static void setTaxRate(Matcher matcher) {
-
-    }
-
-    private static void setFearRate(Matcher matcher) {
     }
 
     private static void showPlayers() {
+        System.out.println(GameMenuController.showPlayers());
+    }
+    private static void showPopularity(){
+        System.out.println(GameMenuController.showPopularity());
+    }
+    private static void showFoodList(){
+        System.out.println(GameMenuController.showFoodList());
+    }
 
+    private static void setFoodRate(String input) {
+        HashMap<String, String> options = InputProcessor.separateInput(input);
+        String rate = "";
+        for (Map.Entry<String, String> option : options.entrySet()) {
+            if (option.getKey().equals("-r")) {
+                rate = option.getValue();
+            } else {
+                System.out.println("invalid option");
+                return;
+            }
+        }
+        if(!rate.matches("-?\\d+"))
+            System.out.println("You should enter a number for rate!");
+        int foodRate=Integer.parseInt(rate);
+        GameMenuMessages message=GameMenuController.setFoodRate(foodRate);
+        switch (message){
+            case INVALID_FOOD_RATE:
+                System.out.println("Enter a number from -2 to 2!");
+                break;
+            case SET_FOOD_RATE_SUCCESSFUL:
+                System.out.println("Food rate is set to "+foodRate);
+        }
 
     }
+    private static void showFoodRate(){
+        System.out.println("Food rate is : "+GameMenuController.showFoodRate());
+    }
+
+
+    private static void setTaxRate(String  input) {
+        HashMap<String, String> options = InputProcessor.separateInput(input);
+        String rate = "";
+        for (Map.Entry<String, String> option : options.entrySet()) {
+            if (option.getKey().equals("-r")) {
+                rate = option.getValue();
+            } else {
+                System.out.println("invalid option");
+                return;
+            }
+        }
+        if(!rate.matches("-?\\d+"))
+            System.out.println("You should enter a number for rate!");
+        int taxRate=Integer.parseInt(rate);
+        GameMenuMessages message=GameMenuController.setFoodRate(taxRate);
+        switch (message){
+            case INVALID_TAX_RATE:
+                System.out.println("Enter a number from -3 to 8!");
+                break;
+            case SET_FOOD_TAX_SUCCESSFUL:
+                System.out.println("Food rate is set to "+taxRate);
+        }
+    }
+    //TODO: do something to prevent duplicate code?
+    private static void showTaxRate(){
+        System.out.println("Tax rate is : "+GameMenuController.showTaxRate());
+    }
+
+
+    private static void showFearRate(){
+        System.out.println("Fear rate is : "+GameMenuController.showFearRate());
+    }
+
+
+    private static void setFearRate(String  input) {
+        HashMap<String, String> options = InputProcessor.separateInput(input);
+        String rate = "";
+        for (Map.Entry<String, String> option : options.entrySet()) {
+            if (option.getKey().equals("-r")) {
+                rate = option.getValue();
+            } else {
+                System.out.println("invalid option");
+                return;
+            }
+        }
+        if(!rate.matches("-?\\d+"))
+            System.out.println("You should enter a number for rate!");
+        int fearRate=Integer.parseInt(rate);
+        GameMenuMessages message=GameMenuController.setFoodRate(fearRate);
+        switch (message){
+            case INVALID_TAX_RATE:
+                System.out.println("Enter a number from -5 to 5!");
+                break;
+            case SET_FOOD_TAX_SUCCESSFUL:
+                System.out.println("Food rate is set to "+fearRate);
+        }
+    }
+
 
     private static void dropBuilding(Matcher matcher) {
 
@@ -80,4 +167,13 @@ public class GameMenu {
     private static void dropTree(Matcher matcher) {
 
     }
+
+    private static void endTurn() {
+
+    }
+
+    private static void leaveGame() {
+
+    }
+
 }
