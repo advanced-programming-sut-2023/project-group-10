65ca378eb0352545c5014240c9482322bf1f5e4b
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-24 22:39:10 +0330

    updated login menu commands

--- StrongholdCrusader/src/main/java/org/example/view/enums/commands/LoginMenuCommands.java
+++ StrongholdCrusader/src/main/java/org/example/view/enums/commands/LoginMenuCommands.java
@@ -4,19 +4,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum LoginMenuCommands {
-    LOGIN("^\\s*user\\s+login\\s*$"),
-    FORGET_PASSWORD("^\\s*forgot\\s+my\\s+password\\s*$"),
+    LOGIN("^\\s*user\\s+login\\s+.+$"),
+    FORGET_PASSWORD("^\\s*forgot\\s+my\\s+password\\s+.+$"),
     LOGOUT("^\\s*user\\s+logout\\s*$");
 
     private final String regex;
 
-    LoginMenuCommands(String regex){
+    LoginMenuCommands(String regex) {
         this.regex = regex;
     }
 
-    public static Matcher getMatcher(String input, LoginMenuCommands command){
+    public static Matcher getMatcher(String input, LoginMenuCommands command) {
         Matcher matcher = Pattern.compile(command.regex).matcher(input);
-        if(matcher.matches()) return matcher;
+        if (matcher.matches()) return matcher;
         else return null;
     }
 }
