1ebe045f15cae2ccaf56c5b172bd42099fd9504e
RozhTagh <119323759+RozhTagh@users.noreply.github.com>
2023-04-24 22:25:12 +0330

    updated profile menu commands

--- StrongholdCrusader/src/main/java/org/example/view/enums/commands/ProfileMenuCommands.java
+++ StrongholdCrusader/src/main/java/org/example/view/enums/commands/ProfileMenuCommands.java
@@ -4,16 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum ProfileMenuCommands {
-    CHANGE_USERNAME("^\\s*profile\\s+change\\s+-u\\s+(?<username>.*)\\s*$"),
-    CHANGE_NICKNAME("^\\s*profile\\s+change\\s+-n\\s+(?<nickname>.*)\\s*$"),
-    CHANGE_PASSWORD("^\\s*profile\\s+change\\s+password\\s+.+$"),
-    CHANGE_EMAIL("^\\s*profile\\s+change\\s+-e\\s+(?<email>.*)\\s*$"),
-    CHANGE_SLOGAN("^\\s*profile\\s+change\\s+slogan\\s+-s\\s+(?<slogan>.*)\\s*$"),
+    CHANGE_USERNAME("^\\s*profile\\s+change\\s+.+$"),
+    CHANGE_NICKNAME("^\\s*profile\\s+change\\s+.+$"),
+    CHANGE_PASSWORD("^\\s*profile\\s+change\\s+password\\s+.+*$"),
+    CHANGE_EMAIL("^\\s*profile\\s+change\\s+.+$"),
+    CHANGE_SLOGAN("^\\s*profile\\s+change\\s+slogan\\s+.+$"),
     REMOVE_SLOGAN("^\\s*profile\\s+remove\\s+slogan\\s*$"),
     DISPLAY_HIGHSCORE("^\\s*profile\\s+display\\s+highscore\\s*$"),
     DISPLAY_RANK("^\\s*profile\\s+display\\s+rank\\s*$"),
     DISPLAY_SLOGAN("^\\s*profile\\s+display\\s+slogan\\s*$"),
-    DISPLAY_PROFILE("^\\s*profile\\s+display\\s*$");
+    DISPLAY_PROFILE("^\\s*profile\\s+display\\s*$"),
+    BACK("^\\s*back\\s*$");
 
     private final String regex;
 
