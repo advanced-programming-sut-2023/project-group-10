ce3f7cba6348cd12d43e7aeb1fd1a5b4d16a5b69
RozhTagh <rozhintn@gmail.com>
2023-06-05 02:25:17 +0330

    updated login menu

--- StrongholdCrusader/src/main/java/org/example/view/LoginMenu.java
+++ StrongholdCrusader/src/main/java/org/example/view/LoginMenu.java
@@ -3,15 +3,18 @@ package org.example.view;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.example.controller.LoginMenuController;
+import org.example.model.SecurityQuestion;
+import org.example.model.User;
 import org.example.view.enums.messages.LoginMenuMessages;
 
 public class LoginMenu extends Application {
@@ -33,10 +36,41 @@ public class LoginMenu extends Application {
         stage.show();
     }
 
-    public void forgetPassword(MouseEvent mouseEvent) {
-        for(int i = 0; i < pane.getChildren().size(); i++){
-            System.out.println(pane.getChildren().get(i));
-        }
+    public void forgetPassword() {
+        pane.getChildren().remove(0);
+        Pane main = new Pane();
+        main.setTranslateX(100);
+        main.setTranslateY(100);
+
+        VBox vbox = new VBox(5);
+        vbox.setAlignment(Pos.CENTER);
+        HBox hBox = new HBox(10);
+        hBox.setAlignment(Pos.CENTER);
+        TextField username = new TextField();
+        username.setPromptText("username");
+        Button show = new Button("show question");
+        hBox.getChildren().addAll(username, show);
+        Label usernameLabel = new Label();
+        usernameLabel.setTranslateX(-50);
+        vbox.getChildren().addAll(hBox, usernameLabel);
+
+        main.getChildren().add(vbox);
+        pane.getChildren().add(main);
+
+        Text question = new Text();
+        TextField answer = new TextField();
+        answer.setPromptText("answer");
+
+        show.setOnMouseClicked(mouseEvent -> {
+            if(username.getText().equals("")) usernameLabel.setText("please provide a username!");
+
+            else if(User.getUserByUsername(username.getText()) != null){
+                String questionNumber = User.getUserByUsername(username.getText()).getQuestionNumber();
+                question.setText(SecurityQuestion.getQuestionByNumber(questionNumber));
+                main.getChildren().addAll(question, answer);
+            }
+            else usernameLabel.setText("This username does not exist!");
+        });
     }
 
     public void submit(MouseEvent mouseEvent) {
