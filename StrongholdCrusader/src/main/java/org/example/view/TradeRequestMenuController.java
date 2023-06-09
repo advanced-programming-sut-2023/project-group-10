35f8d5548def12da76fb7eeb13b2f314a2425c40
MehrazinM <mehrazin82@gmail.com>
2023-06-09 17:16:53 +0330

    trade menu

--- StrongholdCrusader/src/main/java/org/example/view/TradeRequestMenuController.java
+++ StrongholdCrusader/src/main/java/org/example/view/TradeRequestMenuController.java
@@ -1,64 +1,136 @@
 package org.example.view;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import org.example.model.Stronghold;
 import org.example.model.User;
 import org.example.model.game.Government;
+import org.example.model.game.Item;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class TradeRequestMenuController {
-    public static VBox panels = new VBox();
-    private HBox usersAndSelectedOne = new HBox();
-    private ListView<HBox> userList = new ListView<>();
-    private User selectedUser = null;
-    private Label selectedUserLabel = new Label();
+    public Button donate;
+    public Button request;
+    public Label selectedUserLabel;
+    public ListView<HBox> selectedUserResources;
+    public ListView<HBox> usersListView = new ListView<HBox>();
+    public ImageView itemImage;
+    public Label itemNameLabel;
+    public Label itemStorage;
+    public Spinner<Integer> itemAmount;
+    public TextField price;
+    public TextArea messageField;
+    private User selectedUser;
+    private Item selectedItem;
 
 
     @FXML
     public void initialize() {
         showUsers();
-        showSelectedUser();
-        showUsersResources();
+        prepareUserLabel();
 
+    }
+
+    private void prepareUserLabel() {
+        selectedUserLabel.setFont(new Font("Courier new", 24));
+        selectedUserLabel.setTextAlignment(TextAlignment.CENTER);
     }
 
     private void showUsers() {
         ArrayList<HBox> usersBox = new ArrayList<>();
-
+        for (int length = Stronghold.getCurrentBattle().getGovernments().length; length > 0; length--) {
+            usersBox.add(new HBox());
+        }
         for (Government government : Stronghold.getCurrentBattle().getGovernments()) {
+            if (government.getOwner().getUsername().equals(Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getOwner().getUsername()))
+                continue;
             HBox userInfo = new HBox();
             Text username = new Text();
             username.setText(government.getOwner().getUsername());
-            username.setOnMouseClicked(this::selectUser);
+//            username.setOnMouseClicked(this::selectUser);
             userInfo.getChildren().add(username);
             usersBox.add(userInfo);
+            userInfo.setId(Stronghold.getCurrentBattle().getGovernmentByOwnerId(username.getText()).getOwner().getUsername());
+            usersListView.getItems().add(userInfo);
         }
-        userList.setItems((ObservableList<HBox>) usersBox);
-        usersAndSelectedOne.getChildren().add(userList);
-        panels.getChildren().add(usersAndSelectedOne);
-
+        usersListView.setOnMousePressed(event -> {
+            if (event.isPrimaryButtonDown()) {
+                selectedUserLabel.setText(usersListView.getSelectionModel().getSelectedItem().getId());
+                selectedUser = Stronghold.getCurrentBattle().getGovernmentByOwnerId(selectedUserLabel.getText()).getOwner();
+                showUsersResources();
+            }
+        });
 
     }
 
-    private void selectUser(MouseEvent mouseEvent) {
-    }
 
     private void showUsersResources() {
+        ArrayList<HBox> userResources = new ArrayList<>();
+        for (Map.Entry<Item, Double> item : Stronghold.getCurrentBattle().getGovernmentByOwnerId(selectedUser.getUsername()).getItemList().entrySet()) {
+            userResources.add(new HBox());
+        }
+
+        for (Map.Entry<Item, Double> item : Stronghold.getCurrentBattle().getGovernmentByOwnerId(selectedUser.getUsername()).getItemList().entrySet()) {
+            HBox itemInfo = new HBox();
+            Text itemName = new Text(item.getKey().getName());
+            itemName.setFont(new Font("PT Mono", 14));
+            itemInfo.getChildren().add(itemName);
+            userResources.add(itemInfo);
+            itemInfo.setId(item.getKey().getName());
+            selectedUserResources.getItems().add(itemInfo);
+
+        }
+        selectedUserResources.setOnMousePressed(event -> {
+            if (event.isPrimaryButtonDown()) {
+                itemNameLabel.setText(selectedUserResources.getSelectionModel().getSelectedItem().getId());
+                selectedItem = Item.getItemByName(itemNameLabel.getText());
+
+
+                itemStorage.setTextAlignment(TextAlignment.CENTER);
+                itemStorage.setText(Stronghold.getCurrentBattle().getGovernmentByOwnerId
+                        (selectedUser.getUsername()).getItemCount(selectedItem).toString());
+
+
+                SpinnerValueFactory<Integer> valueFactory = //
+                        new SpinnerValueFactory.IntegerSpinnerValueFactory(1, Stronghold.getCurrentBattle().getGovernmentByOwnerId(selectedUser.getUsername()).getItemCount(selectedItem).intValue(), 1);
+                itemAmount.setValueFactory(valueFactory);
+
+
+
+            }
+        });
+
+    }
+
+
+    public void cancel(MouseEvent mouseEvent) throws Exception {
+        new TradeMenuGFX().start(SignupMenu.stage);
+    }
+
+    public void submit(MouseEvent mouseEvent) {
 
 
     }
 
-    private void showSelectedUser() {
+    public void donateSelected(MouseEvent mouseEvent) {
+        donate.setBackground(Background.fill(Color.CORNFLOWERBLUE));
+        request.setBackground(Background.fill(Color.LIGHTGRAY));
 
     }
 
+    public void selectRequest(MouseEvent mouseEvent) {
+        request.setBackground(Background.fill(Color.CORNFLOWERBLUE));
+        donate.setBackground(Background.fill(Color.LIGHTGRAY));
+    }
 
 }
--- StrongholdCrusader/src/main/java/org/example/view/TradeRequestMenuGFX.java
+++ StrongholdCrusader/src/main/java/org/example/view/TradeRequestMenuGFX.java
@@ -3,20 +3,36 @@ package org.example.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class TradeRequestMenuGFX extends Application {
+
+
     @Override
     public void start(Stage stage) throws Exception {
-        Pane requestPane = new FXMLLoader(TradeRequestMenuController.class.getResource("/view/tradeRequestMenu.fxml")).load();
-        requestPane.getChildren().add(TradeRequestMenuController.panels);
+        Pane requestPane = FXMLLoader.load(
+                new URL(TradeRequestMenuGFX.class.getResource("/view/requestMenu" +
+                        ".fxml").toExternalForm()));
+//        requestPane.getChildren().add(TradeRequestMenuController.panels);
         Scene scene = new Scene(requestPane, 1390, 850);
         stage.setScene(scene);
         stage.setTitle("making trade request");
         stage.setFullScreen(true);
         stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args
+        );
+    }
+
+
 }
