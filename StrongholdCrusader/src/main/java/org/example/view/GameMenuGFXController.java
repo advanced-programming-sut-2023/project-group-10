1b1950f99f575bd78a43e229f819159899b0495e
RozhTagh <rozhintn@gmail.com>
2023-06-24 15:15:47 +0330

    completed popularity factors

--- StrongholdCrusader/src/main/java/org/example/view/GameMenuGFXController.java
+++ StrongholdCrusader/src/main/java/org/example/view/GameMenuGFXController.java
@@ -160,6 +160,7 @@ public class GameMenuGFXController {
         mainPane.setBackground(Background.fill(new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/backgrounds/menu.jpeg").toString()))));
 
         VBox first = new VBox(10);
+        first.setTranslateX(-100);
         first.setAlignment(Pos.CENTER);
         Text popularity = new Text("Popularity");
         popularity.setFont(Font.font("Helvetica", FontPosture.ITALIC, 20));
@@ -172,10 +173,19 @@ public class GameMenuGFXController {
             allBuildings();
         });
 
+        int foodRate = Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFoodRate();
+        int taxRate = Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getTaxRate();
+        int fearRate = Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFearRate();
+        int religionRate = Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getReligionCount();
+        int total = foodRate + taxRate + fearRate + religionRate;
+        HBox totalContainer = comingMonth(total);
+        totalContainer.setTranslateX(-20);
+        totalContainer.setTranslateY(50);
+
         VBox second = faces();
-        VBox third = foodAndTax();
-        VBox forth = fearAndReligion();
-        mainPane.getChildren().addAll(first, second, third, forth);
+        VBox third = foodAndTax(foodRate, taxRate);
+        VBox forth = fearAndReligion(fearRate, religionRate);
+        mainPane.getChildren().addAll(first, second, third, forth, totalContainer);
 
         controlBox.getChildren().remove(0);
         controlBox.getChildren().add(0, mainPane);
@@ -184,6 +194,8 @@ public class GameMenuGFXController {
     private static VBox faces(){
         VBox vbox = new VBox(10);
         vbox.setAlignment(Pos.CENTER);
+        vbox.setTranslateX(-30);
+        vbox.setTranslateY(20);
         Rectangle greenFace = new Rectangle(40, 60, new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/greenFace.png").toString())));
         Rectangle redFace = new Rectangle(40, 60, new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/redFace.png").toString())));
         redFace.setTranslateX(greenFace.getTranslateX() + 15);
@@ -192,52 +204,92 @@ public class GameMenuGFXController {
         return vbox;
     }
 
-    private static VBox foodAndTax(){
+    private static VBox foodAndTax(int foodRateNum, int taxRateNum){
         VBox vBox1 = new VBox(10);
         vBox1.setAlignment(Pos.CENTER);
 
         HBox hBox1 = new HBox(5);
         hBox1.setAlignment(Pos.CENTER_LEFT);
-        Text foodRate = new Text(Integer.toString(Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFoodRate()));
-        Rectangle foodFace = new Rectangle(20, 30);
+        Text rate = new Text(Integer.toString(foodRateNum));
+        Rectangle foodFace = new Rectangle(30, 45);
         Text food = new Text("Food");
-        food.setFont(Font.font("Helvetica", 10));
-        hBox1.getChildren().addAll(foodRate, foodFace, food);
+        rate.setFont(Font.font("Helvetica", 15));
+        food.setFont(Font.font("Helvetica", 15));
+        hBox1.getChildren().addAll(rate, foodFace, food);
+        String color;
+        if(foodRateNum > 0) color = "green";
+        else if(foodRateNum == 0) color = "yellow";
+        else color = "red";
+        foodFace.setFill(new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/" + color + "Face.png").toString())));
 
         HBox hBox2 = new HBox(5);
         hBox2.setAlignment(Pos.CENTER_LEFT);
-        Text taxRate = new Text(Integer.toString(Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFoodRate()));
-        Rectangle taxFace = new Rectangle(20, 30);
+        Text taxRate = new Text(Integer.toString(taxRateNum));
+        Rectangle taxFace = new Rectangle(30, 45);
         Text tax = new Text("Tax");
-        tax.setFont(Font.font("Helvetica", 10));
+        taxRate.setFont(Font.font("Helvetica", 15));
+        tax.setFont(Font.font("Helvetica", 15));
         hBox2.getChildren().addAll(taxRate, taxFace, tax);
+        if(taxRateNum > 0) color = "green";
+        else if(taxRateNum == 0) color = "yellow";
+        else color = "red";
+        taxFace.setFill(new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/" + color + "Face.png").toString())));
 
         vBox1.getChildren().addAll(hBox1, hBox2);
         return vBox1;
     }
 
-    private static VBox fearAndReligion(){
+    private static VBox fearAndReligion(int fearRateNum, int religionRateNum){
         VBox vBox2 = new VBox(10);
         vBox2.setAlignment(Pos.CENTER);
 
         HBox fearContainer = new HBox(5);
         fearContainer.setAlignment(Pos.CENTER_LEFT);
-        Text fearRate = new Text(Integer.toString(Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getFearRate()));
-        Rectangle fearFace = new Rectangle(20, 30);
+        Text fearRate = new Text(Integer.toString(fearRateNum));
+        Rectangle fearFace = new Rectangle(30, 45);
         Text fear = new Text("Fear Factor");
-        fear.setFont(Font.font("Helvetica", 10));
+        fear.setFont(Font.font("Helvetica", 15));
+        fearRate.setFont(Font.font("Helvetica", 15));
         fearContainer.getChildren().addAll(fearRate, fearFace, fear);
+        String color;
+        if(fearRateNum > 0) color = "red";
+        else if(fearRateNum == 0) color = "yellow";
+        else color = "green";
+        fearFace.setFill(new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/" + color + "Face.png").toExternalForm())));
 
         HBox religionContainer = new HBox(5);
         religionContainer.setAlignment(Pos.CENTER_LEFT);
-        Text religionRate = new Text(Integer.toString(Stronghold.getCurrentBattle().getGovernmentAboutToPlay().getReligionCount()));
-        Rectangle religionFace = new Rectangle(20, 30);
+        Text religionRate = new Text(Integer.toString(religionRateNum));
+        Rectangle religionFace = new Rectangle(30, 45);
         Text religion = new Text("Religion");
-        religion.setFont(Font.font("Helvetica", 10));
+        religionRate.setFont(Font.font("Helvetica", 15));
+        religion.setFont(Font.font("Helvetica", 15));
         religionContainer.getChildren().addAll(religionRate, religionFace, religion);
+        if(religionRateNum > 0) color = "green";
+        else color = "yellow";
+        religionFace.setFill(new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/" + color + "Face.png").toExternalForm())));
 
         vBox2.getChildren().addAll(fearContainer, religionContainer);
         return vBox2;
+    }
+
+    private static HBox comingMonth(int total){
+        String totalCount;
+        if(total > 0) totalCount = "+" + total;
+        else totalCount = Integer.toString(total);
+        HBox totalContainer = new HBox(10);
+        totalContainer.setAlignment(Pos.CENTER);
+        totalContainer.getChildren().add(new Text("In the coming month: " + totalCount));
+
+        String color;
+        if(total > 0) color = "green";
+        else if(total == 0) color = "yellow";
+        else color = "red";
+
+        ImagePattern imagePattern = new ImagePattern(new Image(GameMenuGFXController.class.getResource("/images/faces/" + color + "Face.png").toString()));
+        Rectangle rectangle = new Rectangle(20, 30, imagePattern);
+        totalContainer.getChildren().add(rectangle);
+        return totalContainer;
     }
 
     private void setBuildingBox(BuildingCategory category){
