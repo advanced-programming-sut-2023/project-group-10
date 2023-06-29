1571b620dbf909354ed2580bd69153b27cd85b21
MehrazinM <mehrazin82@gmail.com>
2023-06-29 17:17:36 +0330

    packet

---
+++ StrongholdCrusader/src/main/java/org/example/connection/PacketParser.java
@@ -0,0 +1,15 @@
+package org.example.connection;
+
+import com.google.gson.Gson;
+
+public class PacketParser {
+
+    public PacketParser(String gson) {
+
+    }
+
+    public PacketParser(Packet packet) {
+
+
+    }
+}
