9cc4593b47369c7f231f970cc7d3911b29ed7711
MehrazinM <mehrazin82@gmail.com>
2023-07-01 00:28:50 +0330

    room

---
+++ Client/src/main/java/org/example/view/RoomChatController.java
@@ -0,0 +1,16 @@
+package org.example.view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class RoomChatController {
+    public Label roomName;
+    public ScrollPane chatScrollPane;
+    public VBox chatBox;
+    public TextField messageField;
+    public Button add;
+    public Button clear;
+}
---
+++ Client/src/main/java/org/example/view/RoomsViewController.java
@@ -0,0 +1,11 @@
+package org.example.view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+
+public class RoomsViewController {
+    public ListView<HBox> chats;
+    public Button addRoomButton;
+
+}
