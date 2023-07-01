e0146770d10aa38f40b0801a914375cdfa9a8c9c
yasna nsh <y.noushirvani@gmail.com>
2023-07-01 14:08:18 +0330

    Update chat

--- Client/src/main/java/org/example/view/chats/PrivateChatGFX.java
+++ Client/src/main/java/org/example/view/chats/PrivateChatGFX.java
@@ -5,14 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.example.connection.Client;
 
 import java.net.URL;
 
 public class PrivateChatGFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Pane chat = FXMLLoader.load(
-                new URL(PrivateChatGFX.class.getResource("/view/privateChat.fxml").toExternalForm()));
+        FXMLLoader loader = new FXMLLoader(new URL(RoomChatGFX.class.getResource("/view/privateChat.fxml").toExternalForm()));
+        Pane chat = loader.load();
+        Client.getInstance().getNotificationReceiver().setChatController(loader.getController());
         if (stage.getScene() == null) {
             Scene scene = new Scene(chat);
             stage.setScene(scene);
--- Client/src/main/java/org/example/view/chats/RoomChatGFX.java
+++ Client/src/main/java/org/example/view/chats/RoomChatGFX.java
@@ -1,18 +1,21 @@
 package org.example.view.chats;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.example.connection.Client;
 
 import java.net.URL;
 
 public class RoomChatGFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Pane chat = FXMLLoader.load(
-                new URL(RoomChatGFX.class.getResource("/view/roomChat.fxml").toExternalForm()));
+        FXMLLoader loader=new FXMLLoader(new URL(RoomChatGFX.class.getResource("/view/roomChat.fxml").toExternalForm()));
+        Pane chat = loader.load();
+        Client.getInstance().getNotificationReceiver().setChatController(loader.getController());
         if (stage.getScene() == null) {
             Scene scene = new Scene(chat);
             stage.setScene(scene);
