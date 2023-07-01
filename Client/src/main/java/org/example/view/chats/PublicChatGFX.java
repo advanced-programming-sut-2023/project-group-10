0ead626ddb04afc794216b0032e4d6b3ba15ccd1
yasna nsh <y.noushirvani@gmail.com>
2023-07-01 13:31:03 +0330

    Minor change

--- Client/src/main/java/org/example/view/chats/PublicChatGFX.java
+++ Client/src/main/java/org/example/view/chats/PublicChatGFX.java
@@ -14,7 +14,7 @@ public class PublicChatGFX extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(new URL(ChatMenuHomeGFX.class.getResource("/view/publicChatMenu.fxml").toExternalForm()));
         Pane root = loader.load();
-        Client.getInstance().getNotificationReceiver().setPublicChatController(loader.getController());
+        Client.getInstance().getNotificationReceiver().setChatController(loader.getController());
         if (stage.getScene() == null) {
             Scene scene = new Scene(root);
             stage.setScene(scene);
